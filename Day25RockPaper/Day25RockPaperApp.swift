a90c3a1dfc60cddd8b81ba53241c7196e8898363
mharari <mharari@varcode.com>
2022-09-09 12:21:38 +0300

    Initial Commit

---
+++ Day25RockPaper/Day25RockPaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day25RockPaperApp.swift
+//  Day25RockPaper
+//
+//  Created by VCM1 on 09/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Day25RockPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
